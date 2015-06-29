2afbf5fe38efecccf7f2e954cf9ac1f989f3e048
MichaelAubry <michael.aubry@autodesk.com>
2015-06-29 14:26:29 -0700

    Update Fibonacci.py
    Added input fields.  Also creates loft sketch profiles now.

--- Fibonacci.py
+++ Fibonacci.py
@@ -5,11 +5,26 @@ import adsk.core, adsk.fusion
 
 app= adsk.core.Application.get()
 design = app.activeProduct
-ui = app.userInterface;
+ui = app.userInterface
 
-#**User Inputs**
-Steps = 15  #How many steps of Fibonacci would you like to plot?
-Length = 2  #How long is the first segment? (cm)
+#**Default User Inputs**
+steps = "5 cm"     #How many steps of Fibonacci would you like to plot?
+                    #(Note, while the steps variable should be unitless, right now our API can't handle unitless number. 
+                    #cm are the absolute units we use on the API side.)
+length = "3 cm"     #How long is the first segment? (cm)
+
+input = steps
+createInput = ui.inputBox('Enter Steps', 'Steps', input)
+if createInput[0]:
+                (input, isCancelled) = createInput
+                unitsMgr = design.unitsManager
+                realSteps = unitsMgr.evaluateExpression(input, unitsMgr.defaultLengthUnits)
+                
+input = length
+createInput = ui.inputBox('Enter Length', 'Length', input)
+if createInput[0]:
+                (input, isCancelled) = createInput
+                realLength = unitsMgr.evaluateExpression(input, unitsMgr.defaultLengthUnits)
 
 #Get root component
 rootComp = design.rootComponent
@@ -20,7 +35,7 @@ sketch = rootComp.sketches.add(rootComp.xYConstructionPlane)
 points = adsk.core.ObjectCollection.create()
 
 # R = total steps to be run thru the For loop
-R = Steps - 2
+R = int(realSteps - 2)
 
 #starting x and y coordiantes
 x = 0
@@ -29,28 +44,31 @@ y = 0
 #Create 1st coordinate
 points.add(adsk.core.Point3D.create(x,y,0))
 
+#Starting Loft Profile Diameter
+loftProfile1 = realLength/8
+
 #starting values for sequence
 fib = 1
 fib1 = 1
 
-#1st fib number
-#print str(fib)
-
 #Create 2nd coordinate
-x = 1 * Length
+x = 1 * realLength
 points.add(adsk.core.Point3D.create(x,y,0))
+ui.messageBox('x: ' + str(x))
 
 #bins for shifting x and y coordinates
 Bin1 = range(0,R,4)
 Bin2 = range(1,R,4)
 Bin3 = range(2,R,4)
 Bin4 = range(3,R,4)
+BinLoft = range(0,R)
 
 for i in range(R):
     fib2 = fib + fib1
     fib = fib1
     fib1 = fib2
-    fibLength = fib*Length #adds the scalar component to  coordinates
+    fibLength = fib*realLength #adds the scalar component to coordinates
+    #ui.messageBox('fiblength: ' + str(fibLength))
 
     if i in Bin1:
         x = x
@@ -68,6 +86,29 @@ for i in range(R):
         x = x + fibLength
         y = y
         points.add(adsk.core.Point3D.create(x,y,0))
+    if i in BinLoft: 
+        loftProfile2 = fibLength/8 #Ending Loft Profile Diameter
 
 # Create the spline.
 sketch.sketchCurves.sketchFittedSplines.add(points)
+
+# Create the Starting Loft Profile
+
+spline1 = sketch.sketchCurves.sketchFittedSplines.item(0)
+
+planeInput = rootComp.constructionPlanes.createInput() # you could also specify the occurrence in the parameter list
+planeInput.setByDistanceOnPath(spline1, adsk.core.ValueInput.createByReal(0))
+plane1 = rootComp.constructionPlanes.add(planeInput)
+
+sketch1 = rootComp.sketches.add(plane1)
+circles = sketch1.sketchCurves.sketchCircles
+circle1 = circles.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), loftProfile1)
+
+# Create the End Loft Sketch Profile
+planeInput.setByDistanceOnPath(spline1, adsk.core.ValueInput.createByReal(1))
+plane2 = rootComp.constructionPlanes.add(planeInput)
+sketch2 = rootComp.sketches.add(plane2)
+
+circles = sketch2.sketchCurves.sketchCircles
+skPosition = sketch2.modelToSketchSpace(spline1.endSketchPoint.geometry)
+circle2 = circles.addByCenterRadius(skPosition, loftProfile2) 
